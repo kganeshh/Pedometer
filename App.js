fea56f1cdc2c477a30916c8a71e3b8387762e111
Sujil Anto Thomas <Sujilnt@gmail.com>
2019-11-10 02:16:40 +0000

    adding some changes

--- App.js
+++ App.js
@@ -1,72 +1,28 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Pedometer } from "expo-sensors";
-import { StyleSheet, Text, View } from "react-native";
+import CircularProgressBar from "./Componnets/CircularProgressBar/CircularProgressBar";
 
-export default class App extends React.Component {
+export default class App extends Component {
   state = {
-    isPedometerAvailable: "checking",
-    pastStepCount: 0,
     currentStepCount: 0
-  };
-
+  }
   componentDidMount() {
     this._subscribe();
   }
-
-  componentWillUnmount() {
-    this._unsubscribe();
-  }
-
   _subscribe = () => {
     this._subscription = Pedometer.watchStepCount(result => {
       this.setState({
         currentStepCount: result.steps
       });
     });
-
-    Pedometer.isAvailableAsync().then(
-      result => {
-        this.setState({
-          isPedometerAvailable: String(result)
-        });
-      },
-      error => {
-        this.setState({
-          isPedometerAvailable: "Could not get isPedometerAvailable: " + error
-        });
-      }
-    );
-
-    const end = new Date();
-    const start = new Date();
-    start.setDate(end.getDate() - 1);
-    Pedometer.getStepCountAsync(start, end).then(
-      result => {
-        this.setState({ pastStepCount: result.steps });
-      },
-      error => {
-        this.setState({
-          pastStepCount: "Could not get stepCount: " + error
-        });
-      }
-    );
-  };
-
-  _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
-  };
-
+  }
   render() {
+    console.log(this.state.currentStepCount);
     return (
       <View style={styles.container}>
-        <Text>
-          Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}
-        </Text>
-        <Text>
-          Steps taken in the last 24 hours: {this.state.pastStepCount}
-        </Text>
-        <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
+        <Text style={styles.instructions}>{this.state.currentStepCount}</Text>
+        <CircularProgressBar />
       </View>
     );
   }
@@ -75,8 +31,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 15,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+    fontSize: 30
+  },
 });
